5a76bb4f7a6c83e6b1f4c24e31d813ac4d38e3bc
Linh <tai-linh.du@arithnea.de>
2019-09-08 15:58:56 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: tai-linh.du
+ *
+ * Created on 8. September 2019, 15:40
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
